9c0ebc275827c33e859863db121d044b1067c6c2
olivier bourgain <olivierbourgain02@gmail.com>
2015-02-21 13:21:44 +0100

    delete old stuff

--- src/main/java/org/elasticsearch/search/aggregations/metrics/sum/InternalSumAccessor.java
+++
@@ -1,9 +0,0 @@
-package org.elasticsearch.search.aggregations.metrics.sum;
-
-public class InternalSumAccessor {
-
-    public static InternalSum create(String name, double sum) {
-        return new InternalSum(name, sum);
-    }
-
-}
