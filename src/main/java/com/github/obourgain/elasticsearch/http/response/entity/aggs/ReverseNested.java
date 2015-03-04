6ca0e08406b7baa37b9f49b9dfb8ea9d1c97fc4c
olivier bourgain <olivierbourgain02@gmail.com>
2015-03-04 11:40:51 +0100

    remove TODO

--- src/main/java/com/github/obourgain/elasticsearch/http/response/entity/aggs/ReverseNested.java
+++ src/main/java/com/github/obourgain/elasticsearch/http/response/entity/aggs/ReverseNested.java
@@ -2,6 +2,4 @@ package com.github.obourgain.elasticsearch.http.response.entity.aggs;
 
 public class ReverseNested extends AbstractSingleBucketAggregation<ReverseNested> {
 
-    // TODO test is as doc do not show that doc_count is returned whereas bu reading the source, it seems it is
-
 }
