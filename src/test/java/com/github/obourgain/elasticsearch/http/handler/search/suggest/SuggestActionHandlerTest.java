76bb92eb2a9ae1770c6e0c8cb989b610277ff580
olivier bourgain <olivierbourgain02@gmail.com>
2015-03-12 10:49:20 +0100

    suggestions not finished

--- src/test/java/com/github/obourgain/elasticsearch/http/handler/search/suggest/SuggestActionHandlerTest.java
+++ src/test/java/com/github/obourgain/elasticsearch/http/handler/search/suggest/SuggestActionHandlerTest.java
@@ -4,15 +4,25 @@ import static org.elasticsearch.search.suggest.SuggestBuilders.completionSuggest
 import static org.elasticsearch.search.suggest.SuggestBuilders.termSuggestion;
 import org.assertj.core.api.Assertions;
 import org.elasticsearch.action.suggest.SuggestRequest;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.common.jackson.dataformat.yaml.snakeyaml.emitter.EmitterException;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.search.suggest.term.TermSuggestionBuilder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import com.github.obourgain.elasticsearch.http.AbstractTest;
 import com.github.obourgain.elasticsearch.http.response.entity.suggest.Completion;
-import com.github.obourgain.elasticsearch.http.response.entity.suggest.Term;
 
 public class SuggestActionHandlerTest extends AbstractTest {
 
+    static XContentType savedContentType;
+
     @Test
+    @Ignore("suggestion are not serialized correctly as of 1.4.4, missing the start_object")
     public void should_suggest() throws Exception {
         createIndex("music");
 
@@ -22,10 +32,8 @@ public class SuggestActionHandlerTest extends AbstractTest {
 
         TermSuggestionBuilder termSuggestionBuilder = termSuggestion("terms-suggest").text("everythign").field("name");
         SuggestRequest suggest = new SuggestRequest("music")
-//                        .suggest(completionSuggestion("song-suggest").text("n").field("name"))
                 .suggest(termSuggestionBuilder);
-        SuggestResponse response = httpClient.suggest(suggest
-        ).get();
+        SuggestResponse response = httpClient.suggest(suggest).get();
 
         Assertions.assertThat(response.getSuggestions()).isNotNull();
         Assertions.assertThat(response.getSuggestions().names()).hasSize(1);
@@ -35,5 +43,17 @@ public class SuggestActionHandlerTest extends AbstractTest {
 
 //        Term term = response.getSuggestions().getTerm("term-suggest");
 //        Assertions.assertThat(term.getOptions()).hasSize(1);
+    }
+
+    @Test(expected = EmitterException.class)
+    public void should_fail_on_yaml() throws Exception {
+        // org.elasticsearch.common.jackson.dataformat.yaml.snakeyaml.emitter.EmitterException: expected DocumentEndEvent, but got <org.elasticsearch.common.jackson.dataformat.yaml.snakeyaml.events.MappingStartEvent(anchor=null, tag=null, implicit=true)>
+        XContentFactory.yamlBuilder()
+                .startObject()
+                .startObject()
+                .endObject()
+                .endObject()
+                ;
+
     }
 }
